7c0b5a14e40acb70bbe29d33642785fb0149d447
Leviathan173 <hft0149@Gmail.com>
2020-07-16 12:50:50 +0800

    优化了文章的获取&&现在会在开始初始化XML文件

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,18 +15,32 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Main extends Print{
+public class Main extends Print {
 
     public static void main(String[] args) throws Exception {
-        while (true){
+        org.jdom2.Document document = null;
+        org.jdom2.Element root;
+        org.jdom2.Element channel = null;
+        try {
+            Println("正在初始化XML文件...");
+            SetupXML();
+            SAXBuilder sb = new SAXBuilder();
+            document = sb.build("rss.xml");
+            root = document.getRootElement();
+            channel = root.getChild("channel");
+            Println("初始化XML文件成功...");
+        } catch (Exception e) {
+            PrintErr("初始化XML文件失败！");
+        }
+        while (true) {
             List<Article> articleList = new ArrayList<Article>();
             DataFinder finder = new DataFinder();
             Document doc = null;
             try {
                 String data = finder.Requester("https://hacg.me/wp/");
-                if(data == null){
-                    for(int i = 0;i<60*60;i++){
-                        Println(i+"/"+(60*60*8));
+                if (data == null) {
+                    for (int i = 0; i < 60 * 60; i++) {
+                        Println(i + "/" + (60 * 60 * 8));
                         Thread.sleep(1000);
                     }
                     continue;
@@ -37,82 +51,88 @@ public class Main extends Print{
             }
             // 数据处理
             assert doc != null;
-            List<Element> articles = finder.GetElementsByTagName(doc,"article");
-            for (Element e:
-                 articles) {
-                String title,content,imgLink,articleLink,magnet = "";
+            List<Element> articles = finder.GetElementsByTagName(doc, "article");
+            for (Element e :
+                    articles) {
+                String title, content, imgLink, articleLink, magnet = "";
                 title = e.getElementsByClass("entry-title").text();
+                if (CheckTitle(channel, title)) {
+                    Println("文章：" + title + " 已存在，跳过...");
+                    continue;
+                }
                 content = e.getElementsByClass("entry-content").text();
                 imgLink = e.getElementsByTag("img").get(0).attr("src");
                 articleLink = e.getElementsByClass("more-link").attr("href");
-                if(!articleLink.equals("") && !articleLink.equals(null)){
+                if (!articleLink.equals("") && !articleLink.equals(null)) {
                     magnet = GetMagnet(finder, Jsoup.parse(finder.Requester(articleLink)));
-                }
-                else{
+                } else {
                     Println("has no magnet in " + articleLink);
                 }
                 articleList.add(new Article(title, content, imgLink, articleLink, magnet));
             }
             // Xml写入
-            try {
-                SetupXML();
-                SAXBuilder sb = new SAXBuilder();
-                org.jdom2.Document document = sb.build("rss.xml");
-                org.jdom2.Element root = document.getRootElement();
-                org.jdom2.Element channel = root.getChild("channel");
-                for (Article a:
-                     articleList) {
-                    if (CheckTitle(channel, a.getTitle())) {
-                        continue;
-                    }
-                    if(a.getTitle().equals("") || a.getTitle().equals(null)){
-                        Println("广告文章，跳过...");
-                        continue;
-                    }
-                    org.jdom2.Element item = new org.jdom2.Element("item");
-                    channel.addContent(item);
-                    org.jdom2.Element titleL = new org.jdom2.Element("title");
-                    titleL.setText(a.getTitle());
-                    item.addContent(titleL);
-                    org.jdom2.Element linkL = new org.jdom2.Element("link");
-                    linkL.setText(a.getLink());
-                    item.addContent(linkL);
-                    org.jdom2.Element descriptionL = new org.jdom2.Element("description");
-                    descriptionL.setText(a.getContent() + "\n<br>" + a.getMagnet() + "<p><img src=\""+a.getImgLink()+"\" /></p>");
-                    item.addContent(descriptionL);
-                    org.jdom2.Element author = new org.jdom2.Element("author");
-                    author.setText("琉璃神社");
-                    item.addContent(author);
-                    org.jdom2.Element pubDate = new org.jdom2.Element("pubDate");
-                    pubDate.setText(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
-                    item.addContent(pubDate);
-                }
-		        org.jdom2.Element lastBuildDate = channel.getChild("lastBuildDate");
-                assert  lastBuildDate != null;
-                lastBuildDate.setText(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
-                //设置生成xml的格式
-                Format format = Format.getCompactFormat();
-                format.setIndent("");
-                //生成不一样的编码
-                format.setEncoding("UTF-8");
-                //4.创建XMLOutputter的对象
-                XMLOutputter outputter = new XMLOutputter(format);
-                //5.利用outputter将document对象转换成xml文档
-                outputter.output(document, new FileOutputStream("rss.xml"));
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (JDOMException e) {
-                e.printStackTrace();
+            if(!WriteXML(articleList, channel, document)){
+                DataFinder.SendErrMail("Rss服务器写入XML文件失败",
+                        "<h1>错误信息</h1><br><div>" +
+                                "错误发生时间：" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) +
+                                "错误类型：写入文件失败" +
+                                "文章："+articleList.toString()+"<br>");
             }
-            for(int i = 0;i<60*60*8;i++){
-                Println(i+"/"+(60*60*8));
+
+            for (int i = 0; i < 60 * 60 * 8; i++) {
+                Println(i + "/" + (60 * 60 * 8));
                 Thread.sleep(1000);
             }
 
         }
 
+    }
+
+    private static boolean WriteXML(List<Article> articleList, org.jdom2.Element channel, org.jdom2.Document document) {
+        try {
+            for (Article a :
+                    articleList) {
+                if (CheckTitle(channel, a.getTitle())) {
+                    continue;
+                }
+                if (a.getTitle().equals("") || a.getTitle().equals(null)) {
+                    Println("广告文章，跳过...");
+                    continue;
+                }
+                org.jdom2.Element item = new org.jdom2.Element("item");
+                channel.addContent(item);
+                org.jdom2.Element titleL = new org.jdom2.Element("title");
+                titleL.setText(a.getTitle());
+                item.addContent(titleL);
+                org.jdom2.Element linkL = new org.jdom2.Element("link");
+                linkL.setText(a.getLink());
+                item.addContent(linkL);
+                org.jdom2.Element descriptionL = new org.jdom2.Element("description");
+                descriptionL.setText(a.getContent() + "\n<br>" + a.getMagnet() + "<p><img src=\"" + a.getImgLink() + "\" /></p>");
+                item.addContent(descriptionL);
+                org.jdom2.Element author = new org.jdom2.Element("author");
+                author.setText("琉璃神社");
+                item.addContent(author);
+                org.jdom2.Element pubDate = new org.jdom2.Element("pubDate");
+                pubDate.setText(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+                item.addContent(pubDate);
+            }
+            org.jdom2.Element lastBuildDate = channel.getChild("lastBuildDate");
+            assert lastBuildDate != null;
+            lastBuildDate.setText(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+            //设置生成xml的格式
+            Format format = Format.getCompactFormat();
+            format.setIndent("");
+            //生成不一样的编码
+            format.setEncoding("UTF-8");
+            //4.创建XMLOutputter的对象
+            XMLOutputter outputter = new XMLOutputter(format);
+            //5.利用outputter将document对象转换成xml文档
+            outputter.output(document, new FileOutputStream("rss.xml"));
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
     }
 
     private static String GetMagnet(DataFinder finder, Document doc) {
@@ -126,7 +146,7 @@ public class Main extends Print{
             while (matcher.find()) {
                 for (int i = 0; i <= matcher.groupCount(); i++) {
                     magent = magent + matcher.group(i);
-                    magent = magent + "\n";
+                    magent = magent + "<br>";
                 }
             }
         }
@@ -135,13 +155,13 @@ public class Main extends Print{
 
     private static boolean CheckTitle(org.jdom2.Element channel, String text) {
         List<org.jdom2.Element> list = channel.getChildren("item");
-        System.out.println("count:" + list.size());
+        // System.out.println("count:" + list.size());
         for (org.jdom2.Element e :
                 list) {
             String title = e.getChild("title").getText();
-            System.out.println("title:" + title);
+            // System.out.println("title:" + title);
             if (title.equals(text)) {
-                System.out.println(title + " is equals " + text);
+                // System.out.println(title + " is equals " + text);
                 return true;
             }
         }
