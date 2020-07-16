74dec7bbc8e153238fcea8e6f9c78a997c451714
Leviathan173 <hft0149@Gmail.com>
2020-07-16 12:49:27 +0800

    删除不需要的重载方法

--- src/main/java/MailSender.java
+++ src/main/java/MailSender.java
@@ -62,55 +62,6 @@ public class MailSender{
         transport.close();
         return true;
     }
-    // 重载一下以防需要不同的收件人
-    public Boolean SendMail(String subject, String content, String myEmailAccount, String myEmailPassword, String myEmailSMTPHost, String receiveMailAccount) throws Exception {
-        Properties props = new Properties();                    // 参数配置
-        props.setProperty("mail.transport.protocol", "smtp");   // 使用的协议（JavaMail规范要求）
-        props.setProperty("mail.smtp.host", myEmailSMTPHost);   // 发件人的邮箱的 SMTP 服务器地址
-        props.setProperty("mail.smtp.auth", "true");            // 需要请求认证
-
-        // SMTP 服务器的端口 (非 SSL 连接的端口一般默认为 25, 可以不添加, 如果开启了 SSL 连接,
-        //                  需要改为对应邮箱的 SMTP 服务器的端口, 具体可查看对应邮箱服务的帮助,
-        //                  QQ邮箱的SMTP(SLL)端口为465或587, 其他邮箱自行去查看)
-        final String smtpPort = "465";
-        props.setProperty("mail.smtp.port", smtpPort);
-        props.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        props.setProperty("mail.smtp.socketFactory.fallback", "false");
-        props.setProperty("mail.smtp.socketFactory.port", smtpPort);
-
-        // 2. 根据配置创建会话对象, 用于和邮件服务器交互
-        Session session = Session.getInstance(props);
-        // 设置为debug模式, 可以查看详细的发送 log
-        session.setDebug(true);
-
-        // 3. 创建一封邮件
-        MimeMessage message = createMimeMessage(session, myEmailAccount, receiveMailAccount, subject, content);
-
-        // 4. 根据 Session 获取邮件传输对象
-        Transport transport = session.getTransport();
-
-        // 5. 使用 邮箱账号 和 密码 连接邮件服务器, 这里认证的邮箱必须与 message 中的发件人邮箱一致, 否则报错
-        //
-        //    PS_01: 如果连接服务器失败, 都会在控制台输出相应失败原因的log。
-        //    仔细查看失败原因, 有些邮箱服务器会返回错误码或查看错误类型的链接,
-        //    根据给出的错误类型到对应邮件服务器的帮助网站上查看具体失败原因。
-        //
-        //    PS_02: 连接失败的原因通常为以下几点, 仔细检查代码:
-        //           (1) 邮箱没有开启 SMTP 服务;
-        //           (2) 邮箱密码错误, 例如某些邮箱开启了独立密码;
-        //           (3) 邮箱服务器要求必须要使用 SSL 安全连接;
-        //           (4) 请求过于频繁或其他原因, 被邮件服务器拒绝服务;
-        //           (5) 如果以上几点都确定无误, 到邮件服务器网站查找帮助。
-        //
-        transport.connect(myEmailAccount, myEmailPassword);
-
-        // 6. 发送邮件, 发到所有的收件地址, message.getAllRecipients() 获取到的是在创建邮件对象时添加的所有收件人, 抄送人, 密送人
-        transport.sendMessage(message, message.getAllRecipients());
-
-        // 7. 关闭连接
-        transport.close();
-        return true;
-    }
 
     public static MimeMessage createMimeMessage(Session session, String sendMail, String receiveMail, String subject, String content) throws Exception {
         // 1. 创建一封邮件
