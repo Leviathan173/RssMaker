b425deaa7a4c657ba50d7863bc860361c2ddec7a
Leviathan173 <hft0149@Gmail.com>
2020-09-04 19:54:51 +0800

    添加了作者和发布时间的字段

--- src/main/java/Article.java
+++ src/main/java/Article.java
@@ -1,11 +1,27 @@
-import java.util.Arrays;
-
 public class Article {
     private String title;
-    private String content;
+    private String publishTime;
+    private String author;
+    private String summary;
     private String imgLink;
     private String link;
     private String magnet;
+
+    public String getPublishTime() {
+        return publishTime;
+    }
+
+    public void setPublishTime(String publishTime) {
+        this.publishTime = publishTime;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 
     public String getTitle() {
         return title;
@@ -15,12 +31,12 @@ public class Article {
         this.title = title;
     }
 
-    public String getContent() {
-        return content;
+    public String getSummary() {
+        return summary;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public void setSummary(String summary) {
+        this.summary = summary;
     }
 
     public String getImgLink() {
@@ -47,25 +63,38 @@ public class Article {
         this.magnet = magnet;
     }
 
-    @Override
-    public String toString() {
-        return "Article{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                ", imgLink='" + imgLink + '\'' +
-                ", link='" + link + '\'' +
-                ", magnets=" + magnet +
-                '}';
-    }
-
-    public Article(String title, String content, String imgLink, String link, String magnet) {
+    /***
+     *
+     * @param title 标题
+     * @param publishTime 发布时间
+     * @param author 作者
+     * @param summary 简介
+     * @param imgLink 图片链接
+     * @param link 文章链接
+     * @param magnet 磁力链接
+     */
+    public Article(String title, String publishTime, String author, String summary, String imgLink, String link, String magnet) {
         this.title = title;
-        this.content = content;
+        this.publishTime = publishTime;
+        this.author = author;
+        this.summary = summary;
         this.imgLink = imgLink;
         this.link = link;
         this.magnet = magnet;
     }
 
+    @Override
+    public String toString() {
+        return "Article{" +
+                "title='" + title + '\'' +
+                ", publishTime='" + publishTime + '\'' +
+                ", author='" + author + '\'' +
+                ", summary='" + summary + '\'' +
+                ", imgLink='" + imgLink + '\'' +
+                ", link='" + link + '\'' +
+                ", magnet='" + magnet + '\'' +
+                '}';
+    }
     public Article() {
     }
 }
